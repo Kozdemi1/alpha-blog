b7b017eec3d0ba0a2782d05b74df58ba2b3f8c54
Kaan git <kaan_ozdmr@hotmail.com>
2017-04-09 20:51:26 +0000

    add sign in upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,10 @@ class UsersController < ApplicationController
     def create
            @user = User.new(user_params)
            if @user.save
+               session[:user_id] = @user.id
+            
                flash[:success] = "Hi, #{@user.username} Welcome to Tarla Blog. "
-               redirect_to articles_path
+               redirect_to articles_path(@user)
            else
                 render 'new'
            end
